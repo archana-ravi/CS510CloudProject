615bfc62763da525fe74139430e974ea8086484a
archana-ravi <archana.ravi488@gmail.com>
2016-03-16 11:55:46 -0700

    Updated file for alternate query 1.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,87 +7,104 @@ import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 
-public class Client
-{
+public class Client {
 
-    static Session session;
-    static ResultSet resultSet;
-    static Row row;
-	
-    public static void main( String[] args ) {
-        Cluster cluster = Cluster.builder()
-                .addContactPoints("10.84.192.98")
-                .build();
-        session = cluster.connect();
-        query1();
+	static Session session;
+	static ResultSet resultSet;
+	static Row row;
+
+	public static void main( String[] args ) {
+		Cluster cluster = Cluster.builder()
+				.addContactPoints("127.0.0.1")
+				.build();
+
+		session = cluster.connect();
+		query1();
 		query2();
 		query3();
 		query4();
 		query5();
-        System.exit(0);
-    }
+		alternateQuery1();
+		System.exit(0);
+	}
 
 	public static void query1() {
 		System.out.println("Executing query 1...");
-
-		String cqlStatement1 = "SELECT COUNT(*) FROM cs510.freeway_data WHERE dqflags = 4;";
+		String cqlStatement1 = "SELECT count(*) FROM cs510.freeway_data where dqflags = 4;";
 		resultSet = session.execute(cqlStatement1);
-		while (resultSet.iterator().hasNext()) {
-			row = resultSet.one();
-			long count = row.getLong("count");
-			System.out.println("\tNumber of speeds > 100 = " + count + "\n");
+		row = resultSet.one();
+		System.out.println("\tNumber of speeds > 100 = " + row.getLong("count") + "\n");
+	}
+
+	public static void alternateQuery1() {
+		System.out.println("\nExecuting alternate query 1...");
+		long count, sum = 0;
+		for (String valueOfDQFlags : Constants.arrayOfPossibleDqflagsWithBit3SetTo1)  {
+			String cqlStatement1 = "SELECT count(*) FROM cs510.freeway_data where dqflags = " + valueOfDQFlags + ";";
+			resultSet = session.execute(cqlStatement1);
+			Row row = resultSet.one();
+			count = row.getLong("count");
+			sum += count;
+			System.out.println("\t\tdqflags value: " + valueOfDQFlags + ",\t Records returned: " + count);
 		}
-
+		System.out.println("\tNumber of speeds > 100 = " + sum + "\n");
 	}
+
 	public static void query2() {
-        System.out.println("Executing query 2...");
+		System.out.println("Executing query 2...");
 
-        String cqlStatement1 = "SELECT sum(volume) as sum FROM cs510.freeway_data WHERE locationtext = 'Foster NB' AND starttime >= '2011-09-21 00:00:00+0000' AND starttime < '2011-09-22 00:00:00+0000' ALLOW FILTERING;";
-        resultSet = session.execute(cqlStatement1);
-        row = resultSet.one();
-        int volume = row.getInt("sum");
-        System.out.println("\tTotal volume for the station Foster NB on 21st Sept 2011 is: " + volume + "\n");
-
+		String cqlStatement1 = "SELECT sum(volume) as sum FROM cs510.freeway_data WHERE locationtext = 'Foster NB' AND starttime >= '2011-09-21 00:00:00+0000' AND starttime < '2011-09-22 00:00:00+0000' ALLOW FILTERING;";
+		resultSet = session.execute(cqlStatement1);
+		row = resultSet.one();
+		int volume = row.getInt("sum");
+		System.out.println("\tTotal volume for the station Foster NB on 21st Sept 2011 is: " + volume + "\n");
 	}
+
 	public static void query3() {
-        System.out.println("Executing query 3...");
+		System.out.println("Executing query 3...");
 
-        String cqlStatement3 = "SELECT length FROM cs510.station_data WHERE locationtext = 'Foster NB'";
-        resultSet = session.execute(cqlStatement3);
-        row = resultSet.one();
-        float length = row.getFloat("length");
-        //System.out.println("Length of Foster NB station: " + length);
+		String cqlStatement3 = "SELECT length FROM cs510.station_data WHERE locationtext = 'Foster NB'";
+		resultSet = session.execute(cqlStatement3);
+		row = resultSet.one();
+		float length = row.getFloat("length");
+		System.out.println("\tLength of station 'Foster NB' is: " + length);
 
-        String cqlStatement1 = "SELECT sum(speed) as sum FROM cs510.freeway_data WHERE locationtext = 'Foster NB' AND starttime >= '2011-09-21 07:00:00+0000' AND starttime <='2011-09-21 09:00:00+0000' ALLOW FILTERING;";
-        resultSet = session.execute(cqlStatement1);
-        row = resultSet.one();
-        int sumSpeed7To9AM = row.getInt("sum");
-        cqlStatement1 = "SELECT count(speed) as count FROM cs510.freeway_data WHERE locationtext = 'Foster NB' AND starttime >= '2011-09-21 07:00:00+0000' AND starttime <='2011-09-21 09:00:00+0000' ALLOW FILTERING;";
-        resultSet = session.execute(cqlStatement1);
-        row = resultSet.one();
-        long countSpeed7To9AM = row.getLong("count");
-        System.out.println("\tAvg travel time for 7-9 am is: " + ((float)sumSpeed7To9AM)/countSpeed7To9AM * 3600 + " seconds");
+		String cqlStatement1 = "SELECT sum(speed) as sum FROM cs510.freeway_data WHERE locationtext = 'Foster NB' AND starttime >= '2011-09-22 07:00:00+0000' AND starttime <= '2011-09-22 09:00:00+0000' ALLOW FILTERING;";
+		resultSet = session.execute(cqlStatement1);
+		row = resultSet.one();
+		int sumSpeed7To9AM = row.getInt("sum");
+		cqlStatement1 = "SELECT count(speed) as count FROM cs510.freeway_data WHERE locationtext = 'Foster NB' AND starttime >= '2011-09-22 07:00:00+0000' AND starttime <= '2011-09-22 09:00:00+0000' ALLOW FILTERING;";
+		resultSet = session.execute(cqlStatement1);
+		row = resultSet.one();
+		long countSpeed7To9AM = row.getLong("count");
+		float avgSpeed7To9AM = ((float) sumSpeed7To9AM)/countSpeed7To9AM;
+		System.out.println("\n\tSum: " + sumSpeed7To9AM + ", Count: " + countSpeed7To9AM);
+		System.out.println("\tAvg speed (7-9am): " + avgSpeed7To9AM);
+		System.out.println("\tAvg travel time for 7-9 am is: " + (length / avgSpeed7To9AM) * 3600 + " seconds");
 
-        String cqlStatement2 = "SELECT sum(speed) as sum FROM cs510.freeway_data WHERE locationtext = 'Foster NB' AND starttime >= '2011-09-21 16:00:00+0000' AND starttime <'2011-09-21 18:00:00+0000' ALLOW FILTERING;";
-        resultSet = session.execute(cqlStatement2);
-        row = resultSet.one();
-        int sumSpeed4To6PM = row.getInt("sum");
-        cqlStatement2 = "SELECT count(speed) as count FROM cs510.freeway_data WHERE locationtext = 'Foster NB' AND starttime >= '2011-09-21 16:00:00+0000' AND starttime <'2011-09-21 18:00:00+0000' ALLOW FILTERING;";
-        resultSet = session.execute(cqlStatement2);
-        row = resultSet.one();
-        long countSpeed4To6PM = row.getLong("count");
-        System.out.println("\tAvg travel time for 4-5 pm is: " + ((float) sumSpeed4To6PM)/countSpeed4To6PM * 3600 + " seconds\n");
-    }
+		String cqlStatement2 = "SELECT sum(speed) as sum FROM cs510.freeway_data WHERE locationtext = 'Foster NB' AND starttime >= '2011-09-22 16:00:00+0000' AND starttime <= '2011-09-22 18:00:00+0000' ALLOW FILTERING;";
+		resultSet = session.execute(cqlStatement2);
+		row = resultSet.one();
+		int sumSpeed4To6PM = row.getInt("sum");
+		cqlStatement2 = "SELECT count(speed) as count FROM cs510.freeway_data WHERE locationtext = 'Foster NB' AND starttime >= '2011-09-22 16:00:00+0000' AND starttime <= '2011-09-22 18:00:00+0000' ALLOW FILTERING;";
+		resultSet = session.execute(cqlStatement2);
+		row = resultSet.one();
+		long countSpeed4To6PM = row.getLong("count");
+		float avgSpeed4To6PM = (float) sumSpeed4To6PM/countSpeed4To6PM;
+		System.out.println("\n\tSum: " + sumSpeed4To6PM + ", Count: " + countSpeed4To6PM);
+		System.out.println("\tAvg speed (4-6pm): " +avgSpeed4To6PM);
+		System.out.println("\tAvg travel time for 4-6 pm is: " + (length / avgSpeed4To6PM) * 3600 + " seconds\n");
+	}
 	public static void query4() {
 		System.out.println("Executing query 4...");
 
-        int i = 0;
-        int endStationId = 0;
-        List<LocationText> usPath = new ArrayList<LocationText>();
-        LocationText lt;
-        String destinationLocationText = "";
+		int i = 0;
+		int endStationId = 0;
+		List<LocationText> usPath = new ArrayList<LocationText>();
+		LocationText lt;
+		String destinationLocationText = "";
 
-        String cqlStatement1 = "SELECT stationid, locationtext, upstream, downstream FROM  cs510.station_data WHERE shortdirection = 'N';";
+		String cqlStatement1 = "SELECT stationid, locationtext, upstream, downstream FROM  cs510.station_data WHERE shortdirection = 'N';";
 		ResultSet s = session.execute(cqlStatement1);
 		while (s.iterator().hasNext()) {
 			Row r = s.one();
@@ -138,25 +155,24 @@ public class Client
 				System.out.print(usPath.get(i).dslocationText +"--->");
 		}
 		System.out.println(destinationLocationText);
-
 	}
 
 	public static void query5() {
-        System.out.println("\nExecuting query 5...");
-        String cqlStatement = "SELECT length FROM cs510.station_data WHERE stationid = 1140";
-        resultSet = session.execute(cqlStatement);
-        row = resultSet.one();
-        System.out.println("\tOriginal length of station with stationid = 1140 is: " + row.getFloat("length"));
-        System.out.println("\tUpdating length of the station to 2.3");
-        String cqlStatement1 = "UPDATE cs510.station_data SET length = 2.3 WHERE stationid = 1140";
-        session.execute(cqlStatement1);
+		System.out.println("\nExecuting query 5...");
+		String cqlStatement = "SELECT length FROM cs510.station_data WHERE stationid = 1140";
+		resultSet = session.execute(cqlStatement);
+		row = resultSet.one();
+		System.out.println("\tOriginal length of station with stationid = 1140 is: " + row.getFloat("length"));
+		System.out.println("\tUpdating length of the station to 2.3");
+		String cqlStatement1 = "UPDATE cs510.station_data SET length = 2.3 WHERE stationid = 1140";
+		session.execute(cqlStatement1);
 
-        resultSet = session.execute(cqlStatement);
-        row = resultSet.one();
-        System.out.println("\tUpdated length of station with stationid = 1140 is: " + row.getFloat("length"));
+		resultSet = session.execute(cqlStatement);
+		row = resultSet.one();
+		System.out.println("\tUpdated length of station with stationid = 1140 is: " + row.getFloat("length"));
 
-        //Updating length of station 1140 back to original value of 2.14
-        cqlStatement1 = "UPDATE cs510.station_data SET length = 2.14 WHERE stationid = 1140";
-        session.execute(cqlStatement1);
+		//Updating length of station 1140 back to original value of 2.14 so query results are the same for each run.
+		cqlStatement1 = "UPDATE cs510.station_data SET length = 2.14 WHERE stationid = 1140";
+		session.execute(cqlStatement1);
 	}
 }
